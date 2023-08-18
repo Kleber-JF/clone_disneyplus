cefdd646c1424dbc5c316164aa0a8f4e1d060e00
Kleber Fukushima <kleber.jhun@gmail.com>
2023-08-18 15:35:51 -0300

    finalizando a pagina e redimensionamentos

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]')
-    
 
-    for (let i = 0; i < buttons.length; i++) {
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosHeader();
+        } else {
+            exibeElementosHeader();
+        }
+    })
+
+    for (let i = 0; i < buttons.length; i++) {//programação das abas, parte dos shows.
         buttons[i].addEventListener('click', function(button){
             const tabsTarget = button.target.dataset.tabButton;
             const tab = document.querySelector(`[data-tab-id=${tabsTarget}]`);
@@ -14,12 +26,22 @@ document.addEventListener('DOMContentLoaded', function() {
         })
     }
 
-    for (let i =0; i < questions.length; i++) {
+    for (let i =0; i < questions.length; i++) {//programação do faq
         questions[i].addEventListener('click', openCloseAnswer);
     }
 })
 
-function openCloseAnswer(elemento) {
+function ocultaElementosHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementosHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
+
+function openCloseAnswer(elemento) {//programação de abrir e fechar a parte de resposta do faq
     const classe = 'faq__questions__item--is-open';
     const elementoPai = elemento.target.parentNode;
 
@@ -27,14 +49,14 @@ function openCloseAnswer(elemento) {
 
 }
 
-function removeActiveButton() {
+function removeActiveButton() {//programação de tirar o underline das abas
     const buttons = document.querySelectorAll('[data-tab-button]');
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].classList.remove('shows__tabs__button--is-active');
     }
 }
 
-function hideAllTabs() {
+function hideAllTabs() {//programação de trocar o que é exibido nas abas
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
 
     for (let i = 0; i < tabsContainer.length; i++) {
